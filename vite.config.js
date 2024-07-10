0106918aaa544a1075a0ba49b73b0a8fd7f44e1e
YassinEzaouibi <zouibiyassin03@gmail.com>
2024-07-10 18:56:21 +0100

    updates to check render

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist",
+  },
   plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss],
     },
   },
-})
+});
