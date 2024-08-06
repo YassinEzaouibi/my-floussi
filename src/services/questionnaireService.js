0e262f38d71c868c7b5b1e63995fc2ae3979df94
YassinEzaouibi <zouibiyassin03@gmail.com>
2024-08-06 15:26:51 +0100

    update server endpoints

--- src/services/questionnaireService.js
+++ src/services/questionnaireService.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://my-floussi-back.onrender.com/api";
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://my-floussi-back.onrender.com/api";
+// const BASE_URL = "http://localhost:5000/api";
 
 export const fetchAllQuestionnaires = async (token) => {
   try {
